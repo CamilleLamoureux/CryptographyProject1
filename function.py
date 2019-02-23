2c6c09fdb8f5565a10470adcf59cfbf29301a7ec
Camille <291756@supinfo.com>
2019-02-23 19:30:19 +0100

    Structure of the functions

---
+++ function.py
@@ -0,0 +1,28 @@
+# Function that uppercase the plain text and remove spaces
+def convertLetters(text):
+    print("Convert Letters")
+
+
+# Function that generate a key
+def generateKey():
+    print("Generate Key")
+
+
+# Function that verify the generated key
+def keyOK(key):
+    print("key OK")
+
+
+# Function that reorder keyleft
+def shifLeft(keyLeft,i):
+    print("shiftLeft")
+
+
+# Function that reorder keyRight
+def shiftRight(keyRight,i):
+    print("shiftRight")
+
+
+# Function that cipher or decipher the given text
+def algorithm1(text,keyLeft,keyRight,cipher):
+    print("algorithm1")
