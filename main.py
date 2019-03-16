c67485af57b212adaaa7c2a17de6cbbfdd35334c
Camille <291756@supinfo.com>
2019-03-16 19:10:08 +0100

    Auto stash before merge of "master" and "origin/master"

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
-from function import algorithm1, generateKey
+from function import algorithm1,generateKey,convertLetters
 
-print("Bienvenue dans le premier algorithme de cryptage ! \n"
-      "Voulez-vous (1 or 2): \n"
+print("Welcome ! ! \n"
+      "You want to (1 or 2): \n"
       "\t 1- Cipher your own message\n"
       "\t 2- Decipher (you will not choose the message or the key)\n")
 
 choice = int(input("Your choice : "))
 
-# If we want to cipher
-if choice == 1:
+"""# If we want to decipher
+if choice == 2:
     cipher = True
     text = list(
         "PJMNEAJFCDJPMXVMTAQUARKNPZDMWOSEOLMQBGBZTGPTHUHYSOVDLXEYAPUYYNLKAWETEBMLAWBFFPDGVKGKUBTRYDJIVEACLBYVLOLRJROQCHMQHSILAKWJCNDLQSXBOMNKFXSFKDGVDLCWQYDNLH")
@@ -16,8 +16,8 @@ if choice == 1:
     keyRight = list("TWXLPRDZMNUGSAQKJHEBCIFYVO")
     algorithm1(text, keyLeft, keyRight, cipher)
 
-# If we want to decipher
-elif choice == 2:
+# If we want to cipher
+elif choice == 1:
     cipher = False
     text = list(input("Your message : "))
     keyLeft = generateKey()
@@ -26,4 +26,7 @@ elif choice == 2:
 
 # If the user give another number
 else:
-    print("Please try again and make sure your input is 1 or 2.")
+    print("Please try again and make sure your input is 1 or 2.")"""
+
+
+    
